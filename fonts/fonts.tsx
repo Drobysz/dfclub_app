593c87be5f4fdbe0ed903a0468950d3154183235
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-06 23:06:18 +0100

    * Add several weights of main font

---
+++ fonts/fonts.tsx
@@ -0,0 +1,13 @@
+import { Golos_Text } from "next/font/google"; 
+
+export const prime_semibold = Golos_Text({
+	weight: '600'
+});
+
+export const prime_medium = Golos_Text({
+	weight: '500'
+});
+
+export const prime_regular = Golos_Text({
+	weight: '400'
+});

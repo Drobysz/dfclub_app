deaeafbdab6bcec7d2efc7eeb874a7190d4d1038
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-07 01:13:57 +0100

    * Add variable names to the fonts

--- fonts/fonts.tsx
+++ fonts/fonts.tsx
@@ -1,13 +1,16 @@
 import { Golos_Text } from "next/font/google"; 
 
 export const prime_semibold = Golos_Text({
-	weight: '600'
+	weight: '600',
+	variable: '--font-golos-sb'
 });
 
 export const prime_medium = Golos_Text({
-	weight: '500'
+	weight: '500',
+	variable: '--font-golos-md'
 });
 
 export const prime_regular = Golos_Text({
-	weight: '400'
+	weight: '400',
+	variable: '--font-golos-rg'
 });

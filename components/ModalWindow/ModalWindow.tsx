5f45f84ed1cac26051738f3c0286ffa5d8007e98
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-18 19:34:10 +0100

    * Create component ModalWindow
    - A custom white window with a shadow as a border

---
+++ components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,36 @@
+import cn from "classnames";
+import styles from "./mv.module.scss";
+import { MVprops } from "./MW.props";
+
+export const ModalWindow = ({
+	className,
+	children,
+	tag = "span",
+	...props
+}: MVprops)=> {
+	return (
+		<>
+			{tag == "span" &&
+				<span
+					{...props}
+					className={cn(
+						className, styles.mod_window_span_view
+					)}
+				>
+					{children}
+				</span>
+			}
+
+			{tag == "section" &&
+				<section
+					{...props}
+					className={cn(
+						className, styles.mod_window_section_view
+					)}
+				>
+					{children}
+				</section>
+			}
+		</>
+	)
+}

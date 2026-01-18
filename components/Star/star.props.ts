30287d675ab5a0f133c6835ff4752af47258c2fc
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-18 19:27:04 +0100

    * Create component Star
    - It's a star svg icon whose filling of yellow
      depends on the transmitted note as parameter.

---
+++ components/Star/star.props.ts
@@ -0,0 +1,6 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export interface Starprops extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+	note: number;
+	className?: string;
+}

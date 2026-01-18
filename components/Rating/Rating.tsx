22b2adddc104e2e4758bb606aa2cb5427eec19a8
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-18 19:36:05 +0100

    * Create component Rating
    - A component consisting of components Star,
      representing user note.
    
    - It can be
    	- fixed (a note have already been put by user
    		and taken in account)
    	- editable (note can be edited by user)

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { KeyboardEvent, useState } from "react";
+import { Star } from "../Star/Star";
+import { Ratingprops } from "./Ratings.props";
+import cn from "classnames";
+
+export const Rating = ({ isEditable = false, rating, setRating, ...props }: Ratingprops) => {
+	const [hoverRating, setHoverRating] = useState<number | null>(null);
+	const displayRating = hoverRating ?? rating;
+
+	const handleMouseEnter = (i: number) => {
+		if (!isEditable) return;
+			setHoverRating(i);
+	};
+
+	const handleMouseLeave = () => {
+		if (!isEditable) return;
+		setHoverRating(null);
+	};
+
+	const handleClick = (i: number) => {
+		if (!isEditable || !setRating) return;
+		setRating(i);
+	};
+
+	const handleSpace = (e: KeyboardEvent<HTMLSpanElement>, i: number) => {
+		if (e.code !== "Space" || !setRating) return;
+		setRating(i);
+	};
+
+	return (
+		<div className="flex w-fit" {...props}>
+			{Array.from({ length: 5 }, (_, i) => (
+				<span 
+					key={i}
+					className={cn("px-0.5", isEditable && "cursor-pointer")}
+					onMouseEnter={() => handleMouseEnter(i + 1)}
+					onMouseLeave={handleMouseLeave}
+					onClick={() => handleClick(i + 1)}
+					onKeyDown={(e: KeyboardEvent<HTMLSpanElement>) =>
+						isEditable && handleSpace(e, i + 1)
+					}
+
+				>
+					<Star
+						className={"transition-all duration-100"}
+						note={i <= displayRating - 1 ? 5 : 0}
+						tabIndex={isEditable ? 0 : -1}
+					/>
+				</span>
+			))}
+		</div>
+	)
+}
---
+++ components/Rating/Ratings.props.ts
@@ -0,0 +1,5 @@
+export interface Ratingprops{
+    isEditable?: boolean;
+    rating: number;
+    setRating?: (rating: number)=> void;
+}

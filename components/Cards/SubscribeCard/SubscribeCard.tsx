ff6308f6c98e7e8d69a250bf94c4b4ad8d57f391
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-19 15:45:30 +0100

    * Intergrate animation mode
    - Animation mode activates a light animation.
    
    - Component is wrapped in div if animations was not chosen
      otherwise it's wrapped in GleamingFrame with frame
      with light animation.

--- components/Cards/SubscribeCard/SubscribeCard.tsx
+++ components/Cards/SubscribeCard/SubscribeCard.tsx
@@ -1,22 +1,33 @@
-import { GleamingCard } from "@/components/index";
+import { GleamingFrame } from "@/components/index";
 import { 
 	SubscribeBtn,
 	SubscribeTag,
 	Price,
 	SubServices,
-	ShortDescription
+	ShortDescription,
+	SubscribeBody
 } from "./components/index";
 
-export const SubscribeCard = ()=> {
+export const SubscribeCard = ({
+	bgColor = "dark",
+	fit = false
+}: {
+	bgColor?: "green" | "dark",
+	fit?: boolean
+})=> {
+	const Wrapper = bgColor == "dark" ? GleamingFrame : "div";
+
 	return (
-		<GleamingCard className="p-8 h-fit w-99.25 flex flex-col gap-6">
-			<div className="flex flex-col gap-8">
-				<SubscribeTag />
-				<Price />
-			</div>
-			<ShortDescription />
-			<SubscribeBtn />
-			<SubServices />
-		</GleamingCard>
+		<Wrapper className={`rounded-3xl w-${fit ? "fit" : "full"}`}>
+			<SubscribeBody bgColor={bgColor}>
+				<div className="flex flex-col gap-8">
+					<SubscribeTag bgColor={bgColor}/>
+					<Price bgColor={bgColor}/>
+				</div>
+				<ShortDescription bgColor={bgColor}/>
+				<SubscribeBtn />
+				<SubServices bgColor={bgColor}/>
+			</SubscribeBody>
+		</Wrapper>
 	)
 }

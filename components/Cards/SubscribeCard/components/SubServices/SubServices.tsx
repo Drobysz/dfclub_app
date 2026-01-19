8c178bd3e04d0bfab68bdd67531bdd89fb14635b
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-19 15:43:38 +0100

    * Intergrate 2 color mode

--- components/Cards/SubscribeCard/components/SubServices/SubServices.tsx
+++ components/Cards/SubscribeCard/components/SubServices/SubServices.tsx
@@ -3,15 +3,19 @@ import services from "./services.json";
 import cn from "classnames";
 import { prime_medium, prime_regular } from "@/fonts/fonts";
 
-export const SubServices = ()=> {
+export const SubServices = ({ bgColor }: { bgColor: "green" | "dark" })=> {
 	return (
 		<div className="text-white flex flex-col gap-4">
-			<h4 className={prime_medium.className}>
+			<h4 className={cn(
+				prime_medium.className,
+				bgColor == "green" && "text-gray-900"
+			)}>
 				Что включено в подписку: 
 			</h4>
 			<ul className={cn(
 				prime_regular.className,
-				"flex flex-col gap-4"
+				"flex flex-col gap-4",
+				bgColor == "green" && "text-gray-800"
 			)}>
 				{services.map((service, idx)=> (
 					<li

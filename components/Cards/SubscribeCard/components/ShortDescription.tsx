8c1922d1a1ce84be515566e1d19b92c18172ebb3
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-30 11:51:28 +0100

    * Fix bug with font color
    Problem)
       The font color was set incorrectly and didn't correspond
      to component color mode.
    
    Fix)
       To solve the issue instead of ternary condition in className
      a separate conditional style related variable was created.
      Transmitable color is set in terms of color mode.

--- components/Cards/SubscribeCard/components/ShortDescription.tsx
+++ components/Cards/SubscribeCard/components/ShortDescription.tsx
@@ -1,5 +1,7 @@
 export const ShortDescription = ({ bgColor }: { bgColor: "green" | "dark" })=> {
-	const styles = `text-gray-${bgColor == "green" ? '8' : '4'}00`;
+	const styles = bgColor == "green"
+		? "text-gray-800"
+		: "text-gray-400";
 
 	return (
 		<p className={styles}>

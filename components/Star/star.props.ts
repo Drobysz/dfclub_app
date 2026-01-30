11ad0f5cdacc2c6b756234129833c37b9677f7c7
Drobysz <alexanderdrobyschevski@gmail.com>
2026-01-30 11:30:25 +0100

    * Fix error related to hydration mismatch
    Problem) After SSR and hydration gid becomes regenerated
      doesn't match to previously generated id.
    
    Fix) To solve the issue an additional layer of check was added.
         It consists in adding the transmitted id from the parent
         and verifying it. Thus, gid generation occurs only
         on the client side.

--- components/Star/star.props.ts
+++ components/Star/star.props.ts
@@ -3,4 +3,5 @@ import { DetailedHTMLProps, HTMLAttributes } from "react";
 export interface Starprops extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
 	note: number;
 	className?: string;
+	uid: string;
 }

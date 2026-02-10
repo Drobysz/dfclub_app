6a1d11117e7b4102c54ac3321e8c20b551d27898
Drobysz <alexanderdrobyschevski@gmail.com>
2026-02-10 09:42:08 +0100

    * Create main page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+import {
+  Subscription,
+  Feedback,
+  FAQ,
+  Illustration,
+  Referal
+} from "./_sections"
+
+export default function Main() {
+  return (
+    <>
+      <Illustration />
+      <Referal />
+      <Subscription />
+      <Feedback />
+      <FAQ />
+    </>
+  );
+}

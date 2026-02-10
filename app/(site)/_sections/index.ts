8e90b338bfb12dcd814f1a4416ede325057276db
Drobysz <alexanderdrobyschevski@gmail.com>
2026-02-10 09:41:40 +0100

    * Add main page sections indexation

---
+++ app/(site)/_sections/index.ts
@@ -0,0 +1,5 @@
+export * from "./Subscription/Subscription";
+export * from "./FAQ/FAQ";
+export * from "./Feedback/Feedback";
+export * from "./Illustration/Illustration";
+export * from "./Referal/Referal";

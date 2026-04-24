2d66fe59b000140b2b17d4b7e588128396dcde7e
Drobysz <alexanderdrobyschevski@gmail.com>
2026-04-24 15:31:15 +0200

    * Male ProfileIcon inactive

--- components/Comment/components/Profile/Profile.tsx
+++ components/Comment/components/Profile/Profile.tsx
@@ -5,7 +5,9 @@ import cn from "classnames";
 export const Profile = ({ name }: { name: string })=> {
 	return (
 		<div className="flex gap-4 items-center">
-			<ProfileIcon />
+			<ProfileIcon
+				isActive={false}
+			/>
 			<span className={cn(
 				prime_medium.className,
 				"text-gray-900"
